0bf7ec6097159b1f9af604ec6d96ee2239dea22a
Shaurabh2121 <shaurabhkumar2121@gmail.com>
2021-09-30 00:20:16 +0530

    Done
    Done

---
+++ BinaryVedaAssignment/app/src/main/java/com/example/binaryvedaassignment/ProfilePageActivity.kt
@@ -0,0 +1,11 @@
+package com.example.binaryvedaassignment
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ProfilePageActivity  : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile_page)
+    }
+}
